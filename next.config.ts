d264b23ffe0c0184ccdcd49c3eea4b0ed3e0c0d1
Mogreyn <alv87@bk.ru>
2025-02-02 13:08:38 +0200

    Start

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output:"export",
+  basePath: "/My_page",
 };
 
 export default nextConfig;
