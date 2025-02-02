899b0a2748953d4be69cb9ecb93439154ea751a8
Mogreyn <alv87@bk.ru>
2025-02-02 13:34:52 +0200

    styled

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output:"export",
-  basePath: "/My_page",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
