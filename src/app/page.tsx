d6c96082020fb7c85700eb6728f68d9577cbbe71
Mogreyn <alv87@bk.ru>
2025-02-02 14:47:24 +0200

    added new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import styles from "./page.module.scss";
 import AboutMe from "@/components/AboutMe/AboutMe";
 import Tech from "@/components/Tech/Tech";
+import Xp from "@/components/Xp/Xp";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <AboutMe />
       <Tech />
+      <Xp />
     </div>
   );
 }
---
+++ src/components/TC/types/Tc.types.ts
@@ -0,0 +1,3 @@
+export interface TcProps {
+className?: string;
+}
